8d335432cd1b3e697e8c17a2322adb03471dd32a
Valentin Kalchev <valentin@zuant.com>
2021-01-31 21:26:30 +0000

    Created a new iOS project called TransactionsApp and a workspace that uses the TransactionsEngine

---
+++ TransactionsApp/TransactionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionsApp
+//
+//  Created by Valentin Kalchev (Zuant) on 31/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
